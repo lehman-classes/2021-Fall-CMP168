3ad4c81ff1338b4d837130382c95451d7e4f25b6
Hendrix Tavarez <hendrix@lockerfish.com>
2021-08-27 07:58:17 -0400

    playing with variables and computation

---
+++ 2021-08-26/variable/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      int a = 20;
+      int c = 11;
+      int e = a % c ;
+      a = 7;
+      System.out.println("a is " + a);
+      System.out.println("e is " + e);
+    }
+}
