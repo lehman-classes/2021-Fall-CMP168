b5f146fe615c0801f00e2891f567895c20a81f34
Hendrix Tavarez <hendrix@lockerfish.com>
2021-09-01 07:35:24 -0400

    lab

--- 2021-08-31/lab1-21/src/App.java
+++ 2021-08-31/lab1-21/src/App.java
@@ -1,23 +1,5 @@
-import java.util.Scanner;
-
 public class App {
-  public static void main(String[] args) throws Exception {
-
-    char shirtSize = 'L';
-    switch (shirtSize) {
-      case 'M':// eliminate break to also print the next case’s instructions
-      case 'm':
-        System.out.print("Size Medium");
-        break;
-      case 'L':// eliminate break to also print the next case’s instructions  
-      case 'l':
-        System.out.print("Size Large");
-        break;
-      case 'X':// eliminate break to also print the next case’s instructions
-      case 'x':
-        System.out.print("Size XLarge");
-      default:
-        System.out.println("One Size Fits All");
+    public static void main(String[] args) throws Exception {
+        System.out.println("/\\  /\\");
     }
-  }
 }
